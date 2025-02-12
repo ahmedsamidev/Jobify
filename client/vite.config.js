4841845eae3821c2f448b5b76f8e022e4bb22f98
Ahmed Sami <ahmedssamy11555@gmail.com>
2025-02-12 12:21:22 +0200

    modify vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: `https://jobify-rouge-three.vercel.app/api`,
+        target: "http://localhost:5100/api",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
